9cc7e3721298de0fc534e2b34c51297c99a065aa
Kyle Dang <kylekd1@uci.edu>
2025-01-19 20:14:16 -0800

    Style home page

--- client/src/components/VideoCard.jsx
+++ client/src/components/VideoCard.jsx
@@ -4,8 +4,8 @@ const VideoCard = ({ video }) => {
 
     console.log(video.videoid)
   return (
-    <div>
-      <p>{video.title}</p>
+    <div className='flex flex-col justify-center items-center my-8 bg-gray-800 p-8 rounded-lg outline'>
+      <div className='font-archivo text-white my-4 max-w-96 text-center'>{video.title}</div>
       <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.video_id}?si=lJ3L2zcSp0wNlaVq`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   )
