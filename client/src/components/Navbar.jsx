f2eb3b8a2e37483d81f8130659b502c7c345ce3d
Kyle Dang <kylekd1@uci.edu>
2025-02-03 02:33:14 -0800

    Navbar links turn darker when hovering over

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     <nav className='fixed top-0 w-full h-14 flex items-center justify-between text-white bg-black'>
       <ul className='flex items-center'>
         <li><Link to='/'><img src='/hoopflash/hoopflash-logo-black.png' alt='hoopflash logo' className='h-12'/></Link></li>
-        <li><img src='/hoopflash/lightning-bolt.png' className='h-7'/></li>
+        <li><img src='/hoopflash/lightning-bolt.png' alt='lightning bolt'className='h-7'/></li>
       </ul>
       <ul className='flex'>
-        <li><Link to='/teams' className='font-archivo pr-14'>TEAMS</Link></li>
+        <li><Link to='/teams' className='font-archivo pr-14 hover:text-neutral-300'>TEAMS</Link></li>
       </ul>
     </nav>
   )
