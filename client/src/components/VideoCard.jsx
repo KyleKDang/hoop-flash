4a54c2f7947266dd0ce11ff776c73c33ae267228
Kyle Dang <kylekd1@uci.edu>
2025-02-06 14:00:32 -0800

    Add responsiveness to video card

--- client/src/components/VideoCard.jsx
+++ client/src/components/VideoCard.jsx
@@ -4,9 +4,9 @@ const VideoCard = ({ video }) => {
 
     console.log(video.videoid)
   return (
-    <div className='flex flex-col justify-center items-center my-8 bg-gray-800 p-8 rounded-lg outline gap-3'>
-      <div className='font-archivo text-white my-4 max-w-96 text-center'>{video.title}</div>
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.video_id}?si=lJ3L2zcSp0wNlaVq`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className='outline bg-black'></iframe>
+    <div className='w-10/12 lg:w-5/12 flex flex-col justify-center items-center my-4 lg:my-8 bg-gray-800 p-4 lg:p-8 rounded-lg outline gap-3'>
+        <div className='font-archivo text-white my-2 lg:my-4 max-w-96 text-sm lg:text-lg text-center'>{video.title}</div>
+        <iframe src={`https://www.youtube.com/embed/${video.video_id}?si=lJ3L2zcSp0wNlaVq`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className='aspect-video w-full h-full outline bg-black'></iframe>
     </div>
   )
 }
