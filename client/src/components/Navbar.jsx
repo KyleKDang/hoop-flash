92eee513cdf9077c032143da57ab8c404c5ed156
Kyle Dang <kylekd1@uci.edu>
2025-02-08 16:07:00 -0800

    Display navbar buttons based on logged in status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
 
 const Navbar = () => {
-  const accessToken = localStorage.getItem('accessToken')
+  const { user } = useContext(AuthContext)
+  console.log(user)
 
   return (
     <nav className='fixed top-0 w-full h-14 flex items-center justify-between text-white bg-black'>
@@ -14,9 +16,9 @@ const Navbar = () => {
         <li><Link to='/teams' className='font-archivo mr-4 lg:mr-14 hover:text-neutral-300'>TEAMS</Link></li>
       </ul>
       <ul className='flex mr-2 lg:mr-4 gap-2 lg:gap-8'>
-        {accessToken && <li><Link to='/login' className='font-archivo hover:text-neutral-300'>LOGIN</Link></li>}
-        {accessToken && <li><Link to='/signup' className='font-archivo hover:text-neutral-300'>SIGNUP</Link></li>}
-        {!accessToken && <li><Link to='/' className='font-archivo hover:text-neutral-300'>LOGOUT</Link></li>}
+        {(user.userId == 1) && <li><Link to='/login' className='font-archivo hover:text-neutral-300'>LOGIN</Link></li>}
+        {(user.userId == 1) && <li><Link to='/signup' className='font-archivo hover:text-neutral-300'>SIGNUP</Link></li>}
+        {(user.userId != 1) && <li><Link to='/' className='font-archivo hover:text-neutral-300'>LOGOUT</Link></li>}
       </ul>
     </nav>
   )
