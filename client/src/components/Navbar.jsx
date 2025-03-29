5cf0cd8cbe3d04f9cc574e149e5b4a2a52ffa6e9
Kyle Dang <kylekd1@uci.edu>
2025-03-29 14:46:04 -0700

    Fix navbar sizing for mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,16 +21,16 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='fixed top-0 w-full h-14 flex items-center justify-between text-white bg-black'>
+    <nav className='fixed top-0 w-full h-14 flex items-center justify-between text-white bg-black text-sm lg:text-md'>
       <ul className='flex items-center lg:gap-4'>
         <li className='flex items-center mr-2 lg:mr-8'>
-          <Link to='/'><img src='/hoopflash/hoopflash-logo-black.png' alt='hoopflash logo' className='h-12'/></Link>
-          <img src='/hoopflash/lightning-bolt.png' alt='lightning bolt'className='h-7'/>
+          <Link to='/'><img src='/hoopflash/hoopflash-logo-black.png' alt='hoopflash logo' className='h-8 lg:h-12'/></Link>
+          <img src='/hoopflash/lightning-bolt.png' alt='lightning bolt'className='h-5 lg:h-7'/>
         </li>
         <li><Link to='/teams' className='font-archivo mr-4 lg:mr-8 hover:text-neutral-300'>TEAMS</Link></li>
         <li><Link to='/games' className='font-archivo mr-4 lg:mr-8 hover:text-neutral-300'>GAMES</Link></li>
       </ul>
-      <ul className='flex mr-2 lg:mr-4 gap-2 lg:gap-8'>
+      <ul className='flex mr-2 lg:mr-4 gap-4 lg:gap-8'>
         {!loggedIn && <li><Link to='/login' className='font-archivo hover:text-neutral-300'>LOGIN</Link></li>}
         {!loggedIn && <li><Link to='/signup' className='font-archivo hover:text-neutral-300'>SIGNUP</Link></li>}
         {loggedIn && <li><Link to='/' className='font-archivo hover:text-neutral-300' onClick={handleLogout}>LOGOUT</Link></li>}
