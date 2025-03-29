72981b952356c01546438733cf58186066a26520
Kyle Dang <kylekd1@uci.edu>
2025-03-29 14:53:16 -0700

    Fix team card sizing for small device

--- client/src/components/TeamCard.jsx
+++ client/src/components/TeamCard.jsx
@@ -21,7 +21,7 @@ const TeamCard = ({ team, selected }) => {
   return (
     <div className='lg:w-1/3 p-8'>
       <div className='flex flex-col items-center outline bg-gray-800 rounded-lg aspect-square'>
-        <div className='p-10'>
+        <div className='p-6 lg:p-10'>
           <img 
                 src={`/team-logos/${teamLogos[team.id - 1]}`} 
                 alt={`${team.full_name} logo`}
@@ -29,10 +29,10 @@ const TeamCard = ({ team, selected }) => {
             />
         </div>
         <div className='flex flex-col gap-8 pb-4 items-center'>
-          <span className='text-white font-archivo text-2xl'>{team.full_name}</span>
+          <span className='text-white font-archivo text-xl lg:text-2xl'>{team.full_name}</span>
           <button 
               onClick={() => selected ? handleUnselectTeam(team.id) : handleSelectTeam(team.id)} 
-              className='w-72 button text-white font-archivo bg-black p-2 rounded-full hover:outline active:bg-neutral-700 active:outline group'>
+              className='w-60 lg:w-72 button text-white font-archivo bg-black p-2 rounded-full hover:outline active:bg-neutral-700 active:outline group'>
               <div className='flex justify-center items-center gap-2'>
                 <span className='group-active:hidden group-hover:hidden'>{selected ? 'Selected' : 'Select'}</span>
                 <span className='hidden group-active:block group-hover:block'>{selected ? 'Unselect' : 'Select'}</span>
