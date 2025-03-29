3004dc6dc158ac8297bcc55f4730cc0d3edfdf3e
Kyle Dang <kylekd1@uci.edu>
2025-03-29 15:29:00 -0700

    Fix games page for small and medium screens

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -12,22 +12,22 @@ const GameCard = ({ game }) => {
 
   return (
     <>
-    <div className='flex justify-between items-center pt-4 pb-4 pl-20 pr-20 bg-zinc-800 text-white'>
-        <div className='text-2xl text-center text-nowrap pr-20'>{formattedDateTime}</div> 
+    <div className='flex justify-between items-center pt-4 pb-4 pl-4 md:pl-10 lg:pl-20 pr-4 md:pr-10 lg:pr-20 bg-zinc-800 text-white'>
+        <div className='text-xs md:text-xl lg:text-2xl text-center text-nowrap pr-2 md:pr-10 lg:pr-20'>{formattedDateTime}</div> 
         <img 
             src={`/team-logos/${teamLogosAlpha[game.visitor_team.id - 1]}`} 
             alt={`${game.visitor_team.full_name} logo`}
-            className='w-1/6 pr-10'
+            className='w-1/6 lg:pr-10'
         />
         <div className='grid grid-cols-[2fr_1fr_2fr] gap-x-2 w-2/3 items-center text-white text-wrap font-archivo'>
-            <div className='text-2xl text-center'>{game.visitor_team.full_name}</div>
-            <div className='text-lg text-center'>at</div> 
-            <div className='text-2xl text-center'>{game.home_team.full_name}</div>
+            <div className='text-xs md:text-xl lg:text-2xl text-center'>{game.visitor_team.full_name}</div>
+            <div className='text-xs md:text-lg lg:text-lg text-center'>at</div> 
+            <div className='text-xs md:text-xl lg:text-2xl text-center'>{game.home_team.full_name}</div>
         </div>
         <img 
             src={`/team-logos/${teamLogosAlpha[game.home_team.id - 1]}`} 
             alt={`${game.home_team.full_name} logo`}
-            className='w-1/6 pl-10'
+            className='w-1/6 lg:pl-10'
         />
     </div>
     </>
